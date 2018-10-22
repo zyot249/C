e7c2b8a0699752991b23ff17f60b29998ae4ac1a
zyot249 <windnd249@gmail.com>
2018-10-22 09:49:16 +0700

    update

--- Cadvance/undirectedGraph/JRB_uGraph/uGraph.c
+++ Cadvance/undirectedGraph/JRB_uGraph/uGraph.c
@@ -15,7 +15,25 @@ Graph createGraph(){
 /* Add the edge between 2 vertices v1 and v2
  */
 void addEdge(Graph graph,int v1,int v2){
+  JRB node = jrb_find_int(graph,v1);
+  JRB tree;
 
+  if(node == NULL){
+    tree = make_jrb();
+    jrb_insert_int(graph,v1,new_jval_v(tree));
+  }else{
+    tree = (JRB)jval_v(node->val);
+  }
+  jrb_insert_int(tree,v2,new_jval_i(1));
+
+  node = jrb_find_int(graph,v2);
+  if(node == NULL){
+    tree = make_jrb();
+    jrb_insert_int(graph,v2,new_jval_v(tree));
+  }else{
+    tree = (JRB)jval_v(node->jval);
+  }
+  jrb_insert_int(tree,v1,new_jval_i(1));
 }
 
 /* return 1 if v1 and v2 are adjacent
