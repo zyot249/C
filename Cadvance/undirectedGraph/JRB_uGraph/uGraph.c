bfc52aa69ee8a4eb0ca08f85117a4ce2493606b8
zyot249 <windnd249@gmail.com>
2018-10-22 08:52:34 +0700

    update

---
+++ Cadvance/undirectedGraph/JRB_uGraph/uGraph.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../libfdr/jrb.h"
+
+/* Implement Function
+ */
+
+/* Create an undirected graph
+ */
+Graph createGraph(){
+
+}
+
+/* Add the edge between 2 vertices v1 and v2
+ */
+void addEdge(Graph graph,int v1,int v2){
+
+}
+
+/* return 1 if v1 and v2 are adjacent
+ */
+int adjacent(Graph graph,int v1,int v2){
+
+}
+
+/* output is array of adjacent vertices of vertice v
+   return the number of adjacent vertices of v
+ */
+int getAdjacentVertices(Graph graph,int v,int* output){
+
+}
+
+/* Drop an undirected graph
+ */
+void dropGraph(Graph graph){
+
+}
--- Cadvance/undirectedGraph/JRB_uGraph/uGraph.h
+++ Cadvance/undirectedGraph/JRB_uGraph/uGraph.h
@@ -10,3 +10,5 @@ void addEdge(Graph graph, int v1, int v2);
 int adjacent(Graph graph, int v1, int v2);
 int getAdjacentVertices (Graph graph, int v, int* output);
 void dropGraph(Graph graph);
+
+#endif
